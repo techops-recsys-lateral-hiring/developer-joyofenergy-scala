3ad0805dba52b20e3968c3b615e22b238ad325fe
John King <jejking@thoughtworks.com>
2023-08-21 11:28:00 +0200

    makes the json support a little easy to read, and adds an explanatory note

--- src/main/scala/com/tw/energy/controller/JsonSupport.scala
+++ src/main/scala/com/tw/energy/controller/JsonSupport.scala
@@ -9,19 +9,34 @@ import io.circe.{Decoder, Encoder, Error}
 import io.circe.syntax.*
 import io.circe.parser.decode
 
+import java.nio.charset.StandardCharsets
+
+/**
+ * Rudimentary support for handling json marshalling and unmarshalling JSON using Circe. To use,
+ * ensure that `Encoder` and `Decoder` implicits are available for the type you want to handle. For example,
+ * by `import io.circe.generic.auto.*` to gain automatic encoder and decoder generation for case classes.
+ *
+ * Inspired by Heiko Seeberger's code at https://github.com/hseeberger/akka-http-json/tree/master/akka-http-circe/src - but
+ * which doesn't yet support Scala 3 out of the box.
+ */
 trait JsonSupport {
 
-  implicit def jsonToEntityMarshaller[A](implicit encoder: Encoder[A]): ToEntityMarshaller[A] = {
+  implicit def jsonToEntityMarshaller[A](implicit encoder: Encoder[A]): ToEntityMarshaller[A] =
     Marshaller.StringMarshaller.wrap(MediaTypes.`application/json`)((a:A) => a.asJson.noSpaces)
-  }
 
   implicit def jsonFromEntityUnmarshaller[A](implicit decoder: Decoder[A]): FromEntityUnmarshaller[A] = {
-    Unmarshaller.byteStringUnmarshaller.forContentTypes(MediaTypes.`application/json`).andThen(Unmarshaller.withMaterializer[ByteString, A](_ => _ => { bs =>
-      decode[A](bs.decodeString(HttpCharsets.`UTF-8`.nioCharset)).fold(
-        (error: Error) => FastFuture.failed(error),
-        (a:A) => FastFuture.successful(a)
-      )
-    }))
+    Unmarshaller
+      .byteStringUnmarshaller
+      .forContentTypes(MediaTypes.`application/json`)
+      .andThen(stringViaDecoderUnmarshaller)
   }
+
+  private def stringViaDecoderUnmarshaller[A](implicit decoder: Decoder[A]): Unmarshaller[ByteString, A] =
+    Unmarshaller.withMaterializer[ByteString, A](_ => _ => { bs =>
+      decode[A](bs.decodeString(StandardCharsets.UTF_8)).fold(
+        (error: Error) => FastFuture.failed(error),
+        (a: A) => FastFuture.successful(a)
+      )
+    })
 
 }
